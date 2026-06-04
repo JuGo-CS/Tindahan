ec41b03f945fa86053dfb0ee7302d655c12880aa
JuGo-CS <mondejarkenneth64@gmail.com>
2026-06-04 17:06:54 +0800

    feat: initial implementation of header

---
+++ app/components/header.jsx
@@ -0,0 +1,28 @@
+import { useRouter } from "expo-router";
+import { Image, Pressable, Text, View } from "react-native";
+import { useState } from "react";
+
+
+const Header = () => {
+    const router = useRouter();
+  
+    const [storeName, setStoreName] = useState("Lorna");
+
+    return (
+        <View className="bg-[#10B981] px-4 pt-14 pb-3 flex-row justify-between items-center">
+        <Text className="text-white text-3xl font-bold">{storeName}'s Store</Text>
+        {/* <Text className="text-white text-3xl font-bold">Lorna's Store</Text> */}
+        <Pressable
+            onPress={() => router.push("/profile")}
+            className="w-10 h-10 rounded-full bg-gray-300 justify-center items-center"
+        >
+            <Image
+            source={require("../../assets/images/react-logo.png")}
+            className="w-10 h-10 rounded-full"
+            />
+        </Pressable>
+        </View>
+    );
+};
+
+export default Header;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Header from "./components/header.jsx";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-700 text-5xl">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1">
+      <Header />
+      {/* <Text className="text-red-700 text-5xl">Edit app/index.tsx to edit this screen.</Text> */}
     </View>
   );
 }
